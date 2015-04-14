af5756a250f5ced60ea8586c935a22bfe237402b
Dan Vegeto <danvegeto@gmail.com>
2015-04-14 19:42:31 -0400

    added folders for pipelines and java classes, implemented shell script

---
+++ pipelinescript.sh
@@ -0,0 +1,4 @@
+python pipelinescript.py $1 > Output.java 2> /dev/null
+javac Output.java 2> /dev/null
+java -cp . Output
+rm Output.java Output.class parser.out parsetab.py parsetab.pyc 2> /dev/null
