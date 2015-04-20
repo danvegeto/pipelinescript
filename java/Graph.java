e30a1ca3aa32679ab1683b8513300b7eeb2af26b
burakfirik <burakfirik@gmail.com>
2015-04-20 15:18:56 -0400

    added List<Node> getGraphNode() method and also implemented conversion from Table to Graph with Nodes

--- java/Graph.java
+++ java/Graph.java
@@ -1,40 +1,71 @@
 package pipelinescript.java;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-public class Graph {
-	
+import java.util.Map;
+// node1, node2, weight  
+public class Graph extends Table {
+	Table graph;
 	private List<Node> nodes;
-	public Graph() {
-		nodes = new ArrayList<>();
+	public Graph(int numofEdge) {
+		super(numofEdge,3);
+		graph = getTable();
+		//nodes = new ArrayList<>();
+	}
+	public List<Node> getGraphNode(){
+		List<Node> nodes = new ArrayList<>();
+		String [][]edge = graph.getTableString();
+		int numEdge = edge.length;
+		
+		for (String[] ed : edge) {
+			int n1 = Integer.parseInt(ed[0]);
+			int n2 = Integer.parseInt(ed[1]);
+			int weig = Integer.parseInt(ed[2]);
+			
+			if (!nodes.contains(new Node(n1))) {
+				nodes.add(new Node(n1));
+			}
+			if (!nodes.contains(new Node(n2))){
+				nodes.add(new Node(n2));
+			} 
+			Node node1 = nodes.get(nodes.indexOf(new Node(n1)));
+			Node node2 = nodes.get(nodes.indexOf(new Node(n2)));
+			node1.adj.put(node2, weig);
+			// INDIRECTED GRAPH, REMOVE THIS LINE TO MAKE IT DIRECTED GRAPH
+			node2.adj.put(node1, weig); 
+		}
+		return nodes;
 	}
 	
-	public Graph(List<Node> node) {
-		nodes = node;
-	}
-
-
 }
 class Node {
 	String stringData;
-	int intData;
-	List<Node> adj; 
+	int nodeNumber;
+	Map<Node, Integer> adj; 
 	public Node() {
-		intData = -1;
 		stringData = null;
-		adj = new ArrayList<>();
+		adj = new HashMap<>();
 	}
 	
-	public Node(int iData , String sData) {
-		intData = iData;
-		stringData = sData;
+	public Node(int nodeNum) {
+		adj = new HashMap<>();
+		this.nodeNumber= nodeNum;
 	}
-
-
-	public List<Node> getNeighbor() {
+	public Node(int nodeNum , String sData) {
+		adj = new HashMap<>();
+		this.nodeNumber= nodeNum;
+		stringData = sData;
+		
+	}
+	public Map getNeighbor() {
 		return adj;
 	}
 
+	@Override
+	public boolean equals(Object o){
+		Node n = (Node)o;
+		 return n.nodeNumber == this.nodeNumber ? true:false;
+	}
 
 }
--- java/Table.java
+++ java/Table.java
@@ -48,6 +48,9 @@ public class Table {
 	public Table getTable(){
 		return this;
 	}
+	public String[][] getTableString(){
+		return table;
+	}
 	public String get(int i,int j) {
 		 return table[i][j];
 	}
