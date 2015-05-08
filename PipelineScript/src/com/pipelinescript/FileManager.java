0b3a6c9505d5a0233605b7e8d8934b04915a7b03
Dan Vegeto <danvegeto@gmail.com>
2015-05-07 20:34:31 -0400

    implemented file read/write

--- PipelineScript/src/com/pipelinescript/FileManager.java
+++ PipelineScript/src/com/pipelinescript/FileManager.java
@@ -1,25 +1,78 @@
 package com.pipelinescript;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
 public class FileManager 
 {
+	public static final String DATA_DIR = "data";
+	
 	public static Table readTable(String file)
 	{
-		return new Table(read(file));
+		String path = DATA_DIR + "/" + file;
+		
+		return new Table(read(path));
 	}
 	
 	public static void writeTable(String file, Table data)
 	{
-		write(file, data.getRows());
+		String path = DATA_DIR + "/" + file;
+		
+		write(path, data.getRows());
 	}
 	
-	private static String[] read(String file)
+	private static String[] read(String path)
 	{
-		//TODO
-		return null;
+		List<String> lines = new LinkedList<String>();
+		
+		try
+		{
+			BufferedReader reader = new BufferedReader(new FileReader(path));
+		
+			String line = reader.readLine();
+			
+			while(line != null)
+			{
+				lines.add(line);
+				line = reader.readLine();
+			}
+			
+			reader.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return lines.toArray(new String[]{});
 	}
 	
-	private static void write(String file, String[] data)
+	private static void write(String path, String[] data)
 	{
-		//TODO
+		try
+		{
+			PrintWriter writer = new PrintWriter(new FileWriter(path));
+			
+			for(String line : data)
+			{
+				writer.println(line);
+			}
+			
+			writer.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		write("test_1.txt", new String[]{"foo", "bar"});
 	}
 }
