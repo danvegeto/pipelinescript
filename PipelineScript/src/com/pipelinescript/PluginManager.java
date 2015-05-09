14ac5ca23b3e70f2f1af8e7e594b0ca779c79031
burakfirik <burakfirik@gmail.com>
2015-05-09 17:28:59 -0400

    cleaned the code

--- PipelineScript/src/com/pipelinescript/PluginManager.java
+++ PipelineScript/src/com/pipelinescript/PluginManager.java
@@ -8,34 +8,10 @@ import java.util.List;
 
 public class PluginManager
 {	
-	// FOR TEST PURPOSES LEFT 
-	
-	public static void main(String[] args){
-		String pt = "./test/helloPython.py";
-		String[]  arg = new String[]{""};
-	
-		System.out.print(execute(pt, arg));
-	}
-	
-	
-	
-	/*
-	 * Example Usage
-	 * String pythonSource = "./test/PluginManagerTest.py";
-	 * Arguments
-		 String[]  arg = new String[]{"1","2","3"};
-		
-		 Table tableb = execute(pythonSource, arg);
-	 * 
-	 * 
-	 * */
 	public static String execute(String file, String... args)
 	{
-		//TODO
 		List<String> rtn = new ArrayList<>();
-		String rt = "";
 		String ext = file.substring(file.lastIndexOf('.') + 1);
-		String [] lines = null;
 		if (ext.equals("py")){
 			String arguments = "";
 			for (String arg : args) {
@@ -46,21 +22,12 @@ public class PluginManager
 				BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
 				String line = "";
 				while((line = bfr.readLine()) != null) {
-				// System.out.println(line);
 				 rtn.add(line);
 				}          
 				
 			} catch (IOException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-			
-			lines = new String[rtn.size()];
-			
-			for (int i = 0 ; i< rtn.size() ; i++) {
-				rt+=rtn.get(i);
-			}
-			
+			}			
 		}
 		return String.join("\n", rtn);
 	}
