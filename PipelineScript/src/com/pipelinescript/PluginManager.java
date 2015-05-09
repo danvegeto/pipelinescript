d130ac77494d18df75c51cca094e7d53b9de9333
burakfirik <burakfirik@gmail.com>
2015-05-09 17:18:35 -0400

    implemented Table execute(String file, String... args)

--- PipelineScript/src/com/pipelinescript/PluginManager.java
+++ PipelineScript/src/com/pipelinescript/PluginManager.java
@@ -1,10 +1,65 @@
 package com.pipelinescript;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PluginManager
-{
+{	
+	// FOR TEST PURPOSES LEFT 
+	/*
+	public static void main(String[] args){
+		String pt = "./test/PluginManagerTest.py";
+		String[]  arg = new String[]{"1","2","3"};
+		Table tb = execute(pt, arg);
+	}
+	*/
+	
+	
+	/*
+	 * Example Usage
+	 * String pythonSource = "./test/PluginManagerTest.py";
+	 * Arguments
+		 String[]  arg = new String[]{"1","2","3"};
+		
+		 Table tableb = execute(pythonSource, arg);
+	 * 
+	 * 
+	 * */
 	public static Table execute(String file, String... args)
 	{
 		//TODO
-		return null;
+		List<String> rtn = new ArrayList<>();
+		String ext = file.substring(file.lastIndexOf('.') + 1);
+		String [] lines = null;
+		if (ext.equals("py")){
+			String arguments = "";
+			for (String arg : args) {
+				arguments +=" "+ arg;
+			}
+			try {
+				Process pr = Runtime.getRuntime().exec("python "+file+arguments);
+				BufferedReader bfr = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+				String line = "";
+				while((line = bfr.readLine()) != null) {
+				 //System.out.println(line);
+				 rtn.add(line);
+				}          
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			lines = new String[rtn.size()];
+			
+			for (int i = 0 ; i< rtn.size() ; i++) {
+				lines[i] = rtn.get(i);
+			}
+			
+		}
+		return new Table(lines);
 	}
 }
