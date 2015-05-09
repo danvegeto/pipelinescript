e36835d9a9b2a9455d4c2fb1df33f4048a166caf
Dan Vegeto <danvegeto@gmail.com>
2015-05-09 15:44:51 -0400

    fixed bug caused by package statement

--- pls.py
+++ pls.py
@@ -551,8 +551,8 @@ for line in input_code.readlines():
 yacc.parse(output_code)
 
 
-print header
 print "package com.pipelinescript;"
+print header
 print "public class Pipeline\n{"
 print functions
 print "public static void main(String[] args) throws Exception\n{"
